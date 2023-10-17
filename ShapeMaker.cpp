6c25e85ff8b8fdf5a215624dd9a7470541c189d2
WhiTzz <134019164+WhiTzz@users.noreply.github.com>
2023-10-17 11:43:10 -0300

    Update ShapeMaker.cpp
    Bugs Fixed!

--- ShapeMaker.cpp
+++ ShapeMaker.cpp
@@ -5,16 +5,16 @@ int main()
     int lar, choice, i, Void;
     do
     {
-        cout <<" Enter 1 for pyramid, 2 for ladder and 3 for pyramid divided in half" << endl;
+        cout << "Digite 1 para piramide, 2 para escada e 3 para piramide dividida ao meio" << endl;
         cin >> choice;
         if (choice > 3 && choice < 1)
         {
-            cout << "Please, select a valid alternative" << endl;
+            cout << "selecione uma alternativa valida" << endl;
             choice = -100;
         }
     } while (choice == -100);
     int n = 0;
-    cout <<"How tall do you want for your shape?" << endl;
+    cout << "quantos blocos de altura vc deseja?" << endl;
     cin >> lar;
     if (choice == 1)
     {
@@ -43,45 +43,6 @@ int main()
             n++;
             Void = i - 1;
 
-            for (int m = 0; n < Void; m++)
-            {
-                cout << " ";
-            }
-            for (int k = 0; k < n; k++)
-            {
-                cout << "#";
-            }
-            cout << "" << endl;
-        }
-    }
-
-    if (choice == 1)
-    {
-        for (i = lar; i > 0; i--)
-        {
-
-            n++;
-            Void = i - 1;
-
-            for (int m = 0; m < Void; m++)
-            {
-                cout << " ";
-            }
-            for (int k = 0; k < ((n * 2) - 1); k++)
-            {
-                cout << "#";
-            }
-            cout << "" << endl;
-        }
-    }
-    if (choice == 2)
-    {
-        for (i = lar; i > 0; i--)
-        {
-
-            n++;
-            Void = i - 1;
-
             for (int m = 0; m < Void; m++)
             {
                 cout << " ";
@@ -90,6 +51,7 @@ int main()
             {
                 cout << "#";
             }
+            cout << "" << endl;
         }
     }
     if (choice == 3)
